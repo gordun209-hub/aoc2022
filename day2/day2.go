59a57a3557eb65578568d634c2fd70e662e9cd57
gordun209 <ali-han80@hotmail.com>
2022-12-02 21:30:54 +0300

    ww

--- day2/day2.go
+++ day2/day2.go
@@ -2,6 +2,34 @@ package day2
 
 import "fmt"
 
+// Opponent
+// A = Rock
+// B = paper
+// C = scissors
+
+// Me
+// X = Rock
+// Y = Paper
+// Z = Scissors
+
+// Points for choose
+// 1 Rock
+// 2 Paper
+// 3 Scissors
+
+// example guide
+// A Y
+// B X
+// C Z
+
+var point = 0
+
+var (
+	Rock     = 1
+	Paper    = 2
+	Scissors = 3
+)
+
 func Day2() {
 	fmt.Println("Hello from day2")
 }
