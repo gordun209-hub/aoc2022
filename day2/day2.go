571dbef09334d9cbeea62b74d4587f21bc2c01bc
gordun209 <ali-han80@hotmail.com>
2022-12-03 21:23:26 +0300

    finish day 2 part 1

--- day2/day2.go
+++ day2/day2.go
@@ -1,16 +1,10 @@
 package day2
 
-import "fmt"
-
-// Opponent
-// A = Rock
-// B = paper
-// C = scissors
-
-// Me
-// X = Rock
-// Y = Paper
-// Z = Scissors
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 // Points for choose
 // 1 Rock
@@ -22,14 +16,88 @@ import "fmt"
 // B X
 // C Z
 
-var point = 0
+type (
+	shape string
+)
 
-var (
+const (
 	Rock     = 1
 	Paper    = 2
 	Scissors = 3
 )
 
+var point = 0
+
+// Opponent
+// A = Rock
+// B = paper
+// C = scissors
+
+// Me
+// X = Rock
+// Y = Paper
+// Z = Scissors
+
+func getPoint(me, opponent string) int {
+	if me == "X" && opponent == "A" {
+		return 1 + 3
+	} else if me == "Y" && opponent == "B" {
+		return 2 + 3
+	} else if me == "Z" && opponent == "C" {
+		return 3 + 3
+	} else if me == "X" && opponent == "C" {
+		return 1 + 6
+	} else if me == "X" && opponent == "B" {
+		return 1 + 0
+	} else if me == "Y" && opponent == "A" {
+		return 2 + 6
+	} else if me == "Y" && opponent == "C" {
+		return 2 + 0
+	} else if me == "Z" && opponent == "A" {
+		return 3 + 0
+	} else if me == "Z" && opponent == "B" {
+		return 3 + 6
+	}
+	return 0
+}
+
+const input = `A Y
+B X
+C Z`
+
+type Hand struct {
+	opponent string
+	me       string
+}
+
+func placeHands(opponent, me string) Hand {
+	return Hand{opponent, me}
+}
+
+func parseInput() []Hand {
+	hand := []Hand{}
+	file, err := os.Open("input2.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		hand = append(hand, placeHands(string(scanner.Text()[0]), string(scanner.Text()[2])))
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+	return hand
+}
+
 func Day2() {
-	fmt.Println("Hello from day2")
+	handss := parseInput()
+	for _, v := range handss {
+		point += getPoint(v.me, v.opponent)
+	}
+	fmt.Println(point)
+	// parseInput()
 }
