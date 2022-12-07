a20a3d9f7fa0a41ac6e211b0d91bce7790b5b590
gordun209 <ali-han80@hotmail.com>
2022-12-07 14:35:18 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa

--- day7/day7.go
+++ day7/day7.go
@@ -2,10 +2,9 @@ package day7
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
+// first parse input
 var sampleInput = `$ cd /
 $ ls
 dir a
@@ -30,67 +29,26 @@ $ ls
 5626152 d.ext
 7214296 k`
 
-type Folder struct {
-	Name     string
-	Size     int
-	Children []*Folder // nil if not a directory
-}
-
 type File struct {
-	Name string
-	Size int
+	Parent *Dir
+	Name   string
+	Size   int
 }
 
-type Tree struct {
-	file File
-	size int
-	leaf []*Tree
+func (f *File) String()
+func (f *File) GetSize()
+func (f *File) New()
+
+type Dir struct {
+	Name     string
+	Children []*Dir // nil if not a directory
 }
 
-func parseData(s []string) {
-	for _, line := range s {
-		switch {
-		case strings.HasPrefix(line, "$ cd"):
-			dirToMove := parseDirCommand(line)
-			fmt.Println("move to", dirToMove)
-			// fmt.Println("move to ", strings.Fields(line))
-		case strings.HasPrefix(line, "dir"): // TODO append dir to cd
-			// fmt.Println(line, "this is dir")
-		case strings.HasPrefix(line, "$ ls"): // TODO list dirs and files
-			// fmt.Println(line, " is a command ls")
-		case checkIfNumber(line):
-			size, name := getFileInfo(line)
-			fmt.Println("size is", size, "name is ", name)
-		default:
-			fmt.Println("error occured please cover all switch cases")
-		}
-	}
-}
-
-func enumerateTree(tree *Tree) {
-	fmt.Println(tree.file.Name, tree.size)
-	for _, leaf := range tree.leaf {
-		enumerateTree(leaf)
-	}
-}
-
-const sizeLimit = 100000
+func (d *Dir) String()
+func (d *Dir) GetSize()
+func (d *Dir) New()
 
 func Maim() {
-	splitted := strings.Split(sampleInput, "\n")
-	parseData(splitted)
-}
-
-func checkIfNumber(s string) bool {
-	_, e := strconv.Atoi(strings.Fields(s)[0])
-	return e == nil
-}
-
-func parseDirCommand(s string) string {
-	ss := strings.Split(s, " ")
-	return ss[2]
-}
-
-func getFileInfo(s string) (string, string) {
-	return strings.Fields(s)[0], strings.Fields(s)[1]
+	root := Dir{Name: "/"}
+	fmt.Println(root)
 }
